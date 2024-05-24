ffb9014d95a58828b31ee3a0013bf833194b7add
Aaka702 <aakashputhussery.official@gmail.com>
2024-05-24 18:39:44 +0530

    file having all frontend operations

---
+++ ..ScriptsActivate.ps1
@@ -0,0 +1,4 @@
+      PID    PPID    PGID     WINPID   TTY         UID    STIME COMMAND
+     1280    1259    1280      23444  pty0      197610 21:04:41 /usr/bin/PS
+     1258       1    1258      10836  ?         197610 21:04:32 /usr/bin/mintty
+     1259    1258    1259      14636  pty0      197610 21:04:32 /usr/bin/bash
